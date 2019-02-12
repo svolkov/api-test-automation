a741babdde7f36ddf98aafc83a6c7ad73a2140b8
svolkov <s.y.volkov@ukr.net>
2019-02-12 23:14:53 +0200

    First RestAssured test

--- src/test/java/OpenApiTestCase.java
+++ src/test/java/OpenApiTestCase.java
@@ -1,2 +1,15 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class OpenApiTestCase {
+    @Test
+    public void testGetRequest(){
+        given().
+        when().get("https://jsonplaceholder.typicode.com/todos/1").
+                then().assertThat().body("userId", equalTo(1));
+    }
 }
